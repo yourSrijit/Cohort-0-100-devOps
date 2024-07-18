baca07062d5573a292a8b3e030c85736df06e3fc
yourSrijit <berasrijit02@gmail.com>
2024-07-18 22:19:56 +0530

    Peek and Partial API

--- Week12/12.2 Advanced TypeScript APIS/src/index.ts
+++ Week12/12.2 Advanced TypeScript APIS/src/index.ts
@@ -10,3 +10,23 @@ const age=sumOfAge({
 },{name:'kamalika',age:30});
 
 console.log(age);
+
+//1 Pick ✅
+// Pick allows you to create a new type by selecting a set of properties (Keys) from an existing type (Type).
+// Imagine you have a User model with several properties, but for a user profile display, you only need a subset of these properties.
+
+type UpdateProps=Pick<User,'age'| 'name'>    //as this is the subset of User
+
+const display=(user:UpdateProps)=>{
+    console.log(user.age);
+    
+}
+
+// 2.Partial
+// Makes all properties of a type optional, creating a type with the same properties, but each marked as optional.
+type UpdatePropsOptional=Partial<UpdateProps>
+const display1=(user:UpdatePropsOptional)=>{
+    }
+    display1({
+        name:"Srijit"
+    })
