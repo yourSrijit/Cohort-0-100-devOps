57c0f125f1a941f94f6d2f7297710971a2fede44
yourSrijit <berasrijit02@gmail.com>
2024-08-02 22:51:19 +0530

    Prism client complete

--- Week13/Medium-Blog-Clone/backend/src/index.ts
+++ Week13/Medium-Blog-Clone/backend/src/index.ts
@@ -1,29 +1,41 @@
-import { Hono } from 'hono'
-const app = new Hono()
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-app.post('/', (c) => {
-  return c.text('Hello Hono!')
+const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+
+
+// Create the main Hono app
+const app = new Hono();
+
+app.get('/',(C)=>{
+  return C.text("Hi Srijit")  
 })
 
 app.post('/api/v1/signup', (c) => {
-  return c.
-  text('Hello Hono!')
+	return c.text('signup route')
 })
 
 app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
+	return c.text('signin route')
 })
 
 app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
 })
 
-export default app
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+export default app;
